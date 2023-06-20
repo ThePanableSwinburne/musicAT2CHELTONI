d38bc61e688a814383b5e97cd0a0b25aff372565
Panable <directeddan@gmail.com>
2023-06-20 16:39:56 +1000

    Update fetch_data.php

--- fetch_data.php
+++ fetch_data.php
@@ -1,20 +1,31 @@
 <?php
 require_once 'config.php';
 
-// Perform the MySQL query to retrieve the desired data (e.g., MEMORY)
-$query = "SELECT * FROM MEMORY";
-$result = mysqli_query($link, $query);
+// Initialize an array to hold the results from each table
+$data = [];
 
-// Check if the query was successful
-if ($result) {
-    // Fetch the data into an associative array
-    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// Define the tables to fetch data from
+$tables = ['MEMORY', 'STORAGE', 'DISPLAY', 'GPU'];
 
-    // Return the data as JSON response
-    echo json_encode($data);
-} else {
-    echo 'failed to execute query';
+// Loop through each table and fetch the data
+foreach ($tables as $table) {
+    $query = "SELECT * FROM $table";
+    $result = mysqli_query($link, $query);
+
+    // Check if the query was successful
+    if ($result) {
+        // Fetch the data into an associative array
+        $tableData = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        // Add the table data to the main data array
+        $data[$table] = $tableData;
+    } else {
+        echo 'Failed to execute query for table: ' . $table;
+    }
 }
+
+// Return the data as JSON response
+echo json_encode($data);
 
 // Close the database connection
 mysqli_close($link);
