33b0b5a5bb262561a981bc1aeb1f3a286ac4b25a
Panable <directeddan@gmail.com>
2023-06-20 16:34:49 +1000

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Op123#21mgf!');
-define('DB_NAME', 'music_database');
+define('DB_NAME', 'HARDWARE');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
